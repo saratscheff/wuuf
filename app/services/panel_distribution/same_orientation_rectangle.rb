cf9bfb27223fa2925b4ccecc51fb9fef4659d5a7
Pedro Saratscheff <saratscheff@gmail.com>
2022-08-15 17:52:27 -0400

    feat(RoofDistribution): Naive solar panel distribution

---
+++ app/services/panel_distribution/same_orientation_rectangle.rb
@@ -0,0 +1,53 @@
+class PanelDistribution::SameOrientationRectangle
+  attr_reader :roof_dimmension_x,
+              :roof_dimmension_y,
+              :solar_panel_dimmension_x,
+              :solar_panel_dimmension_y
+
+  def calculate_solar_panels_array
+    solar_panels = []
+
+    index_x = 0; index_y = 0
+    while index_y + @solar_panel_dimmension_y <= @roof_dimmension_y
+      while index_x + @solar_panel_dimmension_x <= @roof_dimmension_x
+        solar_panels << solar_panel(index_x, index_y)
+        index_x += @solar_panel_dimmension_x
+      end
+      index_y += @solar_panel_dimmension_y
+      index_x = 0
+    end
+
+    solar_panels
+  end
+
+  private
+
+  def initialize(roof_dimmension_x:, roof_dimmension_y:, solar_panel_dimmension_x:, solar_panel_dimmension_y:)
+    @roof_dimmension_x = roof_dimmension_x
+    @roof_dimmension_y = roof_dimmension_y
+    @solar_panel_dimmension_x = solar_panel_dimmension_x
+    @solar_panel_dimmension_y = solar_panel_dimmension_y
+
+    validate_dimmensions!
+  end
+
+  def validate_dimmensions!
+    dimmensions = [
+      @roof_dimmension_x,
+      @roof_dimmension_y,
+      @solar_panel_dimmension_x,
+      @solar_panel_dimmension_y
+    ]
+
+    fail "All dimmensions need to be positive non-zero values" unless dimmensions.all?(&:positive?)
+  end
+
+  def solar_panel(position_x,position_y)
+    SolarPanelPosition.new(
+      position_x: position_x,
+      position_y: position_y,
+      width: @solar_panel_dimmension_x,
+      height: @solar_panel_dimmension_y
+    )
+  end
+end
---
+++ spec/services/solar_panel_distribution/same_orientation_rectangle_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe PanelDistribution::SameOrientationRectangle do
+  def create_distribution
+    described_class.new(
+      roof_dimmension_x: roof_dimmension_x,
+      roof_dimmension_y: 5,
+      solar_panel_dimmension_x: 1,
+      solar_panel_dimmension_y: 2
+    )
+  end
+
+  context "valid initialization" do
+    let(:roof_dimmension_x) { 3 }
+    let!(:distribution_service) do
+      create_distribution
+    end
+
+    it "returns correct dimmensions" do
+      expect(distribution_service.roof_dimmension_x).to eq 3
+      expect(distribution_service.roof_dimmension_y).to eq 5
+      expect(distribution_service.solar_panel_dimmension_x).to eq 1
+      expect(distribution_service.solar_panel_dimmension_y).to eq 2
+    end
+
+    it "calculates position" do
+      solar_panels = distribution_service.calculate_solar_panels_array
+
+      expect(solar_panels.count).to eq(6)
+      expect(solar_panels.last.position_x).to eq(2)
+      expect(solar_panels.last.position_y).to eq(2)
+    end
+  end
+
+  context "invalid initialization" do
+    let(:roof_dimmension_x) { -2 }
+
+    it "raises error" do
+      expect {create_distribution}.to raise_error(RuntimeError, /All dimmensions need to be positive/)
+    end
+  end
+end
