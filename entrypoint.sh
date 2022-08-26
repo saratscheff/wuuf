3d046d5054290baf41bbf506ac3db71b17fddc15
Pedro Saratscheff <saratscheff@gmail.com>
2022-08-25 22:15:08 -0300

    c rama B

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,14 @@ rm -f /myapp/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
+
+
+asdf
+asdfasdf
+asdfasdfwref
+qerb
+qetb
+qectg
+vq35
+gchw
+46b
